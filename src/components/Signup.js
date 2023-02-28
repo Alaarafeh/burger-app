d0d59841a8b768a5aa645875aa6013947bdd8e1f
rafeh <alaa.rafeh@mail.schwarz>
2023-02-28 23:18:11 +0100

    addd signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,18 +2,29 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-    const [email, setEmail] = useState("");
-    const [username, setUsername] = useState("");
-    const [tel, setTel] = useState("");
+    const [email, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
 
+    const postRegister = () => {
+		fetch("http://localhost:8080/register", {
+			method: "POST",
+			body: JSON.stringify({
+				email,
+				password,
+			})
+		})
+        .then(response => response.json())
+        .then(data => {
+           
+        })
+		.catch((err) => console.error(err));
+	};
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log({ email, username, tel, password });
-        setEmail("");
-        setTel("");
-        setUsername("");
+        postRegister();
+        console.log({ email, password });
         setPassword("");
     };
     const gotoLoginPage = () => navigate("/");
@@ -22,32 +33,14 @@ const Signup = () => {
         <div className='signup__container'>
             <h2>Sign up </h2>
             <form className='signup__form' onSubmit={handleSubmit}>
-                <label htmlFor='email'>Email Address</label>
-                <input
-                    type='email'
-                    name='email'
-                    id='email'
-                    value={email}
-                    required
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <label htmlFor='username'>Username</label>
+                <label htmlFor='email'>Username</label>
                 <input
                     type='text'
-                    id='username'
-                    name='username'
-                    value={username}
+                    id='email'
+                    name='email'
+                    value={email}
                     required
                     onChange={(e) => setUsername(e.target.value)}
-                />
-                <label htmlFor='tel'>Phone Number</label>
-                <input
-                    type='tel'
-                    name='tel'
-                    id='tel'
-                    value={tel}
-                    required
-                    onChange={(e) => setTel(e.target.value)}
                 />
                 <label htmlFor='tel'>Password</label>
                 <input
